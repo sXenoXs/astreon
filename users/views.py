4f80af694e62280efd3d5df07d46a7476803a883
BMBenkyou <178959326+BMBenkyou@users.noreply.github.com>
2024-09-25 22:50:03 +0800

    Added signup logic

--- users/views.py
+++ users/views.py
@@ -1,4 +1,16 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from . forms import CustomSignupForm
 
-class SignUpView(TemplateView):
-    template_name ="SignUp.html" 
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomSignupForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomSignupForm()
+    return render(request, 'registration/SignUp.html', {'form':form})
