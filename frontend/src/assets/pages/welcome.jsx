7860d72ab7136c03cc7ef102d262bd97f488820d
ArryBlack <136603885+ArryBlack@users.noreply.github.com>
2024-11-07 18:18:18 +0530

    set up file structure

---
+++ frontend/src/assets/pages/welcome.jsx
@@ -0,0 +1,10 @@
+export function Welcome() {
+  return (
+    <header>
+      <div className="brand_details">
+        <img src="" alt="" />
+        <span>Astreon Study Buddy</span>
+      </div>
+    </header>
+  );
+}
