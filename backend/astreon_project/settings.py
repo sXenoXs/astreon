7beabcd6b4eb140baddc84255213900ed7061d39
BMBenkyou <178959326+BMBenkyou@users.noreply.github.com>
2024-11-16 00:37:46 +0800

    Initialize new app for the chatbot

--- backend/astreon_project/settings.py
+++ backend/astreon_project/settings.py
@@ -29,6 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "users",
+    "chatbot",
 
     #3rd party
     "corsheaders",
