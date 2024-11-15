791836d427db074ffc534549a45995b3116028e5
Ritaban <ritabanbiswas26@gmail.com>
2024-11-15 19:18:19 +0530

    updated login and sign up page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,22 +1,34 @@
+import { Button } from "../components/button";
 import { Header } from "../components/header";
-import {Footer} from "../components/footer"
-import "./login.css"
-export function Login(){
- return(
+import { Footer } from "../components/footer";
+import "./login.css";
+export function Login() {
+  return (
     <>
-        <Header></Header>
-        <main>
-          
-            <form className="login_form">
-            <h2>Welcome back to Astreon Study Buddy</h2>
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username" placeholder="Username"/>
-                <label htmlFor="username">Password</label>
-                <input type="password" name="password" id="password" placeholder="Password"/>
-            </form>
-        </main>
-        <Footer></Footer>
-    
+      <Header></Header>
+      <main>
+        <form className="login_form">
+          <h2>Welcome back to Astreon Study Buddy</h2>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+          />
+          <label htmlFor="username">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <Button anchorText="Log in" />
+          <p>OR</p>
+          <Button anchorText="Continue with Google" />
+        </form>
+      </main>
+      <Footer></Footer>
     </>
- )
+  );
 }
--- frontend/src/pages/signUp.jsx
+++ frontend/src/pages/signUp.jsx
@@ -1,11 +1,42 @@
+import { Button } from "../components/button";
 import { Header } from "../components/header";
-import {Footer} from "../components/footer"
-export function SignUp(){
-    return(
-        <>
-            <Header buttons={[{anchorText:"Login"}]} />  
-            <Footer/>
-        </>
-    );
+import { Footer } from "../components/footer";
+import "./signup.css";
+export function SignUp() {
+  return (
+    <>
+      <Header buttons={[{ anchorText: "Login", link: "/login" }]} />
+      <form className="signup_form">
+        <h2>Create an account</h2>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" placeholder="Name" />
 
+        <label htmlFor="Email">Email</label>
+        <input type="email" id="email" placeholder="Email" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" placeholder="Password" />
+        <div class="strength-meter">
+          <div class="strength-bar"></div>
+        </div>
+        <p id="strength-text"></p>
+
+        <label htmlFor="confirm_password">Confirm Password</label>
+        <input
+          type="password"
+          id="confirm_password"
+          placeholder="Confirm Password"
+        />
+
+        <label>
+          <input type="checkbox" />
+          By creating an account, you agree to our Terms of Service and Privacy
+          Policy.
+        </label>
+        <Button anchorText="Sign up" />
+      </form>
+
+      <Footer />
+    </>
+  );
 }
