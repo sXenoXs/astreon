be7e9144b71e8e3c4747adbedd5fa07879426584
BMBenkyou <178959326+BMBenkyou@users.noreply.github.com>
2024-11-27 02:20:35 +0800

    Add forms for message and file upload

--- backend/chatbot/forms.py
+++ backend/chatbot/forms.py
@@ -1,25 +1,14 @@
 from django import forms
-from .models import Upload
-import filetype
 
-
-class UploadFileForm(forms.Form):
-    file = forms.FileField()
-
-    def cleaned_data(self):
-        """"
-        Validates the file extension uploaded by the user
-        returns the uploaded file
-        """
-        uploaded_file = self.cleaned_data['file']
-        allowed_extensions = ['pdf','docx', 'jpg', 'jpeg', 'png']
-        file_extension= filetype.guess(uploaded_file)
-    
-
-        if file_extension not in allowed_extensions:
-            raise forms.ValidationError("Unsupported file type. Please try again ")
-        return uploaded_file 
-
-    
+class UploadForm(forms.Form):
+    files = forms.FileField(
+        widget=forms.ClearableFileInput(attrs={'allow_multiple_selected': True}),
+        required=False,  # Make files optional for general chat
+    )
+    question = forms.CharField(
+        max_length=500,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your request or prompt...'}),
+        required=True,
+    )
 
 
